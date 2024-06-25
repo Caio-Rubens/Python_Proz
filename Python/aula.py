953d85d8b75452e061e78a9eec12224447775b0b
Caiorubens <160772890+Caio-Rubens@users.noreply.github.com>
2024-06-25 16:15:09 -0300

    Conteudo da aula.

--- Python/aula.py
+++ Python/aula.py
@@ -1,5 +1,12 @@
 x = 0
+pessoas = []
 
 while x <= 5:
     nome = input('Qual seu nome: ')
+    pessoas.append(nome)
     x += 1
+
+pessoas.sort()
+print(pessoas)
+
+    
---
+++ Python/aula2.py
@@ -0,0 +1,14 @@
+# for i in range(1,11):
+#     print(i)
+
+
+# frutas = ['Banana','Peçego','Pera','Cacau','Uva']
+
+# for fruta in frutas:
+#     print(fruta)
+
+nome_completo = input('Digite seu nome: ')
+
+lista_nome = [str(x) for x in nome_completo.split()]
+
+print(f'Se nome divido: {lista_nome}')
