d470d26b835417c610de319f7934f49a8f5282b0
Caiorubens <160772890+Caio-Rubens@users.noreply.github.com>
2024-07-15 21:41:03 -0300

    Update aula13.py

--- Aulas_Py/aula13.py
+++ Aulas_Py/aula13.py
@@ -1,37 +1,37 @@
-# # dicionario
-# dic_produtos = {'pera':1.45, 'laranja':3.5, 'abacaxi':8.48, 'mexirica':5.78}
+# dicionario
+dic_produtos = {'pera':1.45, 'laranja':3.5, 'abacaxi':8.48, 'mexirica':5.78}
 
-# # pegar um elemento
-# print(dic_produtos['pera'])
+# pegar um elemento
+print(dic_produtos['pera'])
 
-# # editar um elemento valor
-# dic_produtos['abacaxi'] = dic_produtos['abacaxi'] * 0.3
-# print(dic_produtos['abacaxi'])
+# editar um elemento valor
+dic_produtos['abacaxi'] = dic_produtos['abacaxi'] * 0.3
+print(dic_produtos['abacaxi'])
 
-# # quantidade de itens
-# print(len(dic_produtos))
+# quantidade de itens
+print(len(dic_produtos))
 
-# # remover um item
-# dic_produtos.pop('mexirica')
-# print(dic_produtos)
+# remover um item
+dic_produtos.pop('mexirica')
+print(dic_produtos)
 
-# # adicionar um item ao dicionario
-# dic_produtos['pecego'] = 4.5
-# print(dic_produtos)
+# adicionar um item ao dicionario
+dic_produtos['pecego'] = 4.5
+print(dic_produtos)
 
-# # verificar se um item exite no dicionario 
-# if 'pera' in dic_produtos:
-#     print('Existe.')
+# verificar se um item exite no dicionario 
+if 'pera' in dic_produtos:
+    print('Existe.')
 
-# else:
-#     print('Não existe.')
+else:
+    print('Não existe.')
 
-# # verificar seu um valor exite em um dicionario (incomum de usar)
-# if 3.50 in dic_produtos.values():
-#     print('Existe.')
+# verificar seu um valor exite em um dicionario (incomum de usar)
+if 3.50 in dic_produtos.values():
+    print('Existe.')
 
-# else:
-#     print('Não existe.')
+else:
+    print('Não existe.')
 
 nome_item = input('Nome produto: ')
 preco_item = input('Valor do produto: ')
